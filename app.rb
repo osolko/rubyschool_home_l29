ca3fe08662e1af8ed9c0b46688ff7f8c63a0c401
osolko <o.savka@gmail.com>
2019-04-23 09:03:27 +0300

    add index erb

--- app.rb
+++ app.rb
@@ -13,6 +13,9 @@ class  Barber < ActiveRecord::Base
 end
 
 
+# insert new value to table with ActiveRecord
+# Barber.create :name => 'Mike Jonson'
+
 get '/' do
-	erb "Hello!! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
+	erb :index
 end
